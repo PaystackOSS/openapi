3f733df6226399e6a75ba597586f45e176c72a81
Damilola Odujoko <damilola.odujoko@paystack.com>
2025-08-08 16:50:47 +0100

    feat: begin the restructure of the repo for better dev exp

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import fs from 'fs'
+import YAML from 'yaml'
+import path from 'path'
+import open from 'open';
+
+const app = express()
+const port = 3031
+
+const __dirname = new URL('.', import.meta.url).pathname;
+const file = fs.readFileSync(path.join(__dirname, '../temp/paystack.yaml'), 'utf8')
+const oas = YAML.parse(file)
+
+app.use(express.static(path.join(__dirname, '/views')));
+
+app.get('/health', (_, res) => {
+    res.send('Server running fine...')
+})
+
+app.get('/spec', (_, res) => {
+    res.json(oas)
+})
+
+app.listen(port, () => {
+    console.log(`OAS spec is listening on port ${port}`)
+    open(`http://localhost:${port}`);
+})
